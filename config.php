805f602fb40ecb593a6b646d412ce3fbc9dd7ea9
Nur Fadhilah Amaliah <nurfadhilahamaliah8@gmail.com>
2026-07-08 12:02:57 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,15 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 date_default_timezone_set('Asia/Makassar');
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_klinik";
+$host = "sql300.infinityfree.com";
+$user = "if0_42358847";
+$pass = "lEAU2CaBjfo";
+$db   = "if0_42358847_db_klinik";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
